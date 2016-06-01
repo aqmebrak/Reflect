efce10de98c4fa5f4bee170c8847f1d586d9a11b
Ugo El Koubi <eu201468@etu.unice.fr>
2016-06-01 16:39:15 +0200

    clean

--- mirror.php
+++ mirror.php
@@ -7,14 +7,14 @@
     <script src="js/jquery.simpleWeather.min.js"></script>
     <script src="js/weather.js"></script>
     <script src="js/bootstrap.min.js"></script>
-<!--    <script>-->
-<!--        window.setInterval(function(){-->
-<!--            if(localStorage.getItem("reload")) {-->
-<!--                location.reload();-->
-<!--                localStorage.removeItem("reload");-->
-<!--            }-->
-<!--        }, 5000);-->
-<!--    </script>-->
+    <script>
+        window.setInterval(function(){
+            if(localStorage.getItem("reload")) {
+                location.reload();
+                localStorage.removeItem("reload");
+            }
+        }, 5000);
+    </script>-->
 </head>
 <body>
 <?php
