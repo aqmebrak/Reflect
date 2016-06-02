6587ecc44557f0fec1641e47b37932bd45dec8b2
= <cappe@polytech.unice.fr>
2016-06-02 18:38:13 +0200

    Ajout video basique

--- mirror.php
+++ mirror.php
@@ -9,21 +9,20 @@ if(!isset($_SESSION['uid']))
 <head>
     <meta charset="UTF-8">
     <title>Mirror</title>
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
-            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+	<script src="lib/jquery.rotate.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js"></script>
     <script src="js/weather.js"></script>
     <script src="js/clock.js"></script>
     <link href="css/mirror.css" rel="stylesheet">
     <script src="lib/bootstrap.min.js"></script>
-    <script src="lib/draggable.min.js"></script>
-    <script src="lib/jquery.rotate.js"></script>
+    
     <script src="lib/jClocksGMT.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPjTonWpMnclazoTL22ibJOdAPyb4CmaA"
             type="text/javascript"></script>
     <link rel="stylesheet" href="css/jClocksGMT.css">
     <link rel="stylesheet" type="text/css" href="css/weather.css">
-    <script src="js/draggable.min.js"></script>
+    <script src="lib/draggable.min.js"></script>
     <script src="js/video.js"></script>
     <link rel="stylesheet" type="text/css" href="css/meteo.css">
 
@@ -45,12 +44,13 @@ if(!isset($_SESSION['uid']))
 
 <?php include_once('./widgets/weather.php'); ?>
 
-
-<div id="center">
+<div class="widget grabbable draggable">
+<div id="player" ></div>
 </div>
+
 <div id="rightPanel">
 	<ul>
-		<li><div id="maquillageIcon" class="widgetIcon" ><div></li>
+		<li onclick="launchVideo()"><div id="maquillageIcon" class="widgetIcon" ><div></li>
 		<li><a >Lorem</a></li>
 		<a href="config/weather-config.php"><li><div id="ecrouIcon" class="widgetIcon"><div></li></a>
 		<a href="index.php"><li><div id="exitIcon" class="widgetIcon"><div></li></a>
