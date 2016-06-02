e2fd7ae0afc6714f765ccaeb8a1bdce22750545b
= <cappe@polytech.unice.fr>
2016-06-02 16:53:59 +0200

    Ajout deconnexion utilisateur menu a droite

--- mirror.php
+++ mirror.php
@@ -43,8 +43,8 @@
 	<ul>
 		<li><div id="maquillageIcon" class="widgetIcon" ><div></li>
 		<li><a >Lorem</a></li>
-		<li><a >Ipsum</a></li>
 		<a href="config/weather-config.php"><li><div id="ecrouIcon" class="widgetIcon"><div></li></a>
+		<a href="index.php"><li><div id="exitIcon" class="widgetIcon"><div></li></a>
 	</ul>
 </div>
 
