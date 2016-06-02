417bb83f3884f71920bf53bd0a4e6748be5e4a8b
Matthieu Perrin <pm403977@polytech.unice.fr>
2016-06-02 10:38:28 +0200

    ajout meteo au php !

--- mirror.php
+++ mirror.php
@@ -3,30 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Mirror</title>
-    <script src="lib/jquery-2.2.4.min.js"></script>
-    <script src="lib/jquery.simpleWeather.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
+            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js"></script>
     <script src="js/weather.js"></script>
-
-	<link href="css/mirror.css" rel="stylesheet">
+    <link href="css/mirror.css" rel="stylesheet">
     <script src="lib/bootstrap.min.js"></script>
+    <script src="js/draggable.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/meteo.css">
+
     <script>
-        window.setInterval(function(){
-            if(localStorage.getItem("reload")) {
+        window.setInterval(function () {
+            if (localStorage.getItem("reload")) {
                 location.reload();
                 localStorage.removeItem("reload");
             }
         }, 5000);
-
     </script>
 </head>
 <body>
 <?php
 if (isset($_POST['uid']))
-	echo $_POST['uid'];
+    echo $_POST['uid'];
 ?>
-<div id="weather"></div>
-
-<div class="grabbable" > Grab me !</div>
+<?php include_once('./widgets/weather.php'); ?>
 </body>
 
 
