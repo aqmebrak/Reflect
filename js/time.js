be73de4caac0975f0064f1c857b513065a4a72ab
Ugo El Koubi <eu201468@etu.unice.fr>
2016-06-02 11:17:27 +0200

    time in progress

---
+++ js/time.js
@@ -0,0 +1,4 @@
+function initTime() {
+    var s = '<iframe src="http://free.timeanddate.com/clock/i581xf3t/n181/fn6/fs28/fcfff/tct/pct/ftb/bas2/bacfff/tt0/tw1/tm1/th2" frameborder="0" width="467" height="42" allowTransparency="true"></iframe>';
+    $("#time").html(s);
+}   
--- mirror.php
+++ mirror.php
@@ -7,6 +7,7 @@
             integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js"></script>
     <script src="js/weather.js"></script>
+    <script src="js/time.js"></script>
     <link href="css/mirror.css" rel="stylesheet">
     <script src="lib/bootstrap.min.js"></script>
     <script src="js/draggable.min.js"></script>
@@ -17,16 +18,18 @@
             if (localStorage.getItem("reload")) {
                 location.reload();
                 localStorage.removeItem("reload");
+                var $draggable = $('.draggable').draggabilly({
+                    // options...
+                }
             }
         }, 5000);
     </script>
 </head>
 <body>
-<?php
-if (isset($_POST['uid']))
-    echo $_POST['uid'];
-?>
+
+<?php include_once('./widgets/time.php'); ?>
 <?php include_once('./widgets/weather.php'); ?>
+
 </body>
 
 
