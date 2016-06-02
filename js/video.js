a52196101e3548cf1d75886b1880005aa8bbe550
Matthieu Perrin <pm403977@polytech.unice.fr>
2016-06-02 21:26:05 +0200

    ajout close vidéo

--- js/video.js
+++ js/video.js
@@ -7,32 +7,38 @@ function launchVideo() {
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
-    var player;
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-            height: '390',
-            width: '640',
-            videoId: 'SgSAlH6iMTM',
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
-        });
-    }
-
-    // 4. The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-        event.target.playVideo();
-    }
-
-    // 5. The API calls this function when the player's state changes.
-    //    The function indicates that when playing a video (state=1),
-    //    the player should play for six seconds and then stop.
-    var done = false;
-    function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-            done = true;
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'SgSAlH6iMTM',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
         }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        done = true;
     }
+}
+
+//ferme le bloc vidéo
+function closeFrame() {
+    var element = document.getElementById("player");
+    element.parentNode.removeChild(element);
+}
--- mirror.php
+++ mirror.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if(!isset($_SESSION['uid']))    
-    $_SESSION['uid']=$_POST['uid'];
+if (!isset($_SESSION['uid']))
+    $_SESSION['uid'] = $_POST['uid'];
 ?>
 
 <!DOCTYPE html>
@@ -9,17 +9,18 @@ if(!isset($_SESSION['uid']))
 <head>
     <meta charset="UTF-8">
     <title>Mirror</title>
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-	<script src="lib/jquery.rotate.js"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
+            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script src="lib/jquery.rotate.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js"></script>
     <script src="js/weather.js"></script>
     <script src="js/clock.js"></script>
     <link href="css/mirror.css" rel="stylesheet">
     <script src="lib/bootstrap.min.js"></script>
-    
+
     <script src="lib/jClocksGMT.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPjTonWpMnclazoTL22ibJOdAPyb4CmaA"
-            type="text/javascript"></script>
+    <!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPjTonWpMnclazoTL22ibJOdAPyb4CmaA"
+            type="text/javascript"></script>-->
     <link rel="stylesheet" href="css/jClocksGMT.css">
     <link rel="stylesheet" type="text/css" href="css/weather.css">
     <script src="lib/draggable.min.js"></script>
@@ -44,17 +45,32 @@ if(!isset($_SESSION['uid']))
 
 <?php include_once('./widgets/weather.php'); ?>
 
-<div class="widget grabbable draggable">
-<div id="player" ></div>
+<div id="video" class="widget grabbable draggable">
+    <span style="color:white; font-size: 2.2em;"><a onclick="closeFrame()">&#10006;</a></span>
+    <br/>
+    <div id="player"></div>
 </div>
 
 <div id="rightPanel">
-	<ul>
-		<li onclick="launchVideo()"><div id="maquillageIcon" class="widgetIcon" ><div></li>
-		<li><a >Lorem</a></li>
-		<a href="config/weather-config.php"><li><div id="ecrouIcon" class="widgetIcon"><div></li></a>
-		<a href="index.php"><li><div id="exitIcon" class="widgetIcon"><div></li></a>
-	</ul>
+    <ul>
+        <li onclick="launchVideo()">
+            <div id="maquillageIcon" class="widgetIcon">
+                <div>
+        </li>
+        <li><a>Lorem</a></li>
+        <a href="config/weather-config.php">
+            <li>
+                <div id="ecrouIcon" class="widgetIcon">
+                    <div>
+            </li>
+        </a>
+        <a href="index.php">
+            <li>
+                <div id="exitIcon" class="widgetIcon">
+                    <div>
+            </li>
+        </a>
+    </ul>
 </div>
 
 </body>
