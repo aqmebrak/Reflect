c8128d2c4160363667bc3a3298cf6f697011e9e0
Matthieu Perrin <pm403977@polytech.unice.fr>
2016-06-02 14:30:19 +0200

    weather

--- widgets/weather.php
+++ widgets/weather.php
@@ -1,18 +1,19 @@
 <div id="weather" class="draggable"></div>
 
 <?php
-    $jsonString = file_get_contents('database/'.$_POST['uid'].'.json');
-    $data = json_decode($jsonString, true);
+$jsonString = file_get_contents('database/' . $_POST['uid'] . '.json');
+$data = json_decode($jsonString, true);
 
-    $city = $data['weather']['location'];
-    $degree = $data['weather']['degree'];
+$city = $data['weather']['location'];
+$degree = $data['weather']['degree'];
 ?>
 
 <script>
-    window.onload = function() {
+    window.onload = function () {
         var city = "<?php echo $city; ?>";
         var degree = "<?php echo $degree; ?>";
-        initWeather(city,degree);
+        initWeather(city, degree);
+        initTime();
         var $draggable = $('.draggable').draggabilly({
             // options...
         })
