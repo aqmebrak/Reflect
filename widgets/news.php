0540ea9a234274ec83e61c9068cd33f0114f592b
Melchor Jérémy <melchor.pro@gmail.com>
2016-06-08 13:30:42 +0200

    Fix news

--- widgets/news.php
+++ widgets/news.php
@@ -10,7 +10,6 @@
         border: 2px white solid;
         border-radius: 10px;
         width: 40%;
-        height: 10%;
         color: white;
         font-size: x-large;
         font-weight: bold;
@@ -32,16 +31,17 @@ $xml = new SimpleXmlElement($content);
 $news = [];
 
 foreach($xml->channel->item as $entry) {
-    echo $entry->title;
-    array_push($news,$entry->title);
+    array_push($news,(string) $entry->title);
 }
 
-
-//print_r($news);
+$final = json_encode($news);
 ?>
 
+<!------------------------------------------------->
+<!------ Script for displaying the newsfeed ------->
+<!------------------------------------------------->
 <script>
-    var listNewsTitle = "<?php echo json_encode($news); ?>";
+    listNewsTitle = <?php echo $final; ?>;
     var length = listNewsTitle.length;
     // First call so we don't wait the first setInterval
     $('#news').find("marquee").text(listNewsTitle[0]);
@@ -54,38 +54,12 @@ foreach($xml->channel->item as $entry) {
     $("#news").mousedown(function () {
         document.cookie = "currentWidget=news";
     });
-</script>
-
-<!------------------------------------------------->
-<!-- Script for loading the newsfeed from an url -->
-<!------------------------------------------------->
-<!--<script>
-    $(document).ready(function() {
-        var url = "<?php echo $url; ?>";
-        getRSSFeed(url);
-    });
-
-    function getRSSFeed(url) {
-        $.get(url, function (data) {
-            var listNews = [];
-            $(data).find("item").find("title").each(function () {
-                listNews.push($(this).text());
-            });
-            var length = listNews.length;
-            // First call so we don't wait the first setInterval
-            $('#news').find("marquee").text(listNews[0]);
-            setInterval(function() {
-                    var number = 1 + Math.floor(Math.random() * length);
-                    $('#news').find("marquee").text(listNews[number]);
-                },
-                15000); // every 10 second
-        });
-    }
 
     /* Put this to make the widget draggable */
     var $draggable = $('.draggable').draggabilly({
         // options...
     });
-</script>-->
+</script>
+
 
 
