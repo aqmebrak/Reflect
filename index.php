f9a4543886bd87c02a3a5ce99dd107832fec80c1
Matthieu Perrin <pm403977@polytech.unice.fr>
2016-06-09 13:55:51 +0200

    fix la session à lindex

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-if (isset($_SESSION['uid']))
-    session_destroy();
 session_start();
+if (isset($_SESSION['uid'])) {
+    session_destroy();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,13 +12,15 @@ session_start();
     <script src="lib/jquery-2.2.4.min.js"></script>
     <script src="js/index.js"></script>
     <script src="lib/bootstrap.min.js"></script>
-	<script src="js/cursor.js"></script>
+    <script src="js/cursor.js"></script>
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/index.css" rel="stylesheet">
     <link href="css/hover.css" rel="stylesheet">
+
 </head>
 <body>
+
 <img
     class="logo"
     src="images/logo.png"/>
