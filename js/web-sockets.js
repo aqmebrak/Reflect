a38d2d04f5c449c39a2f1cc3e80a830d14c89a2c
Matthieu Perrin <pm403977@polytech.unice.fr>
2016-06-14 16:14:11 +0200

    finale version tshirt

--- js/web-sockets.js
+++ js/web-sockets.js
@@ -25,7 +25,7 @@
             var handRight = {X: 0};
             var handLeft = {X: 0};
             var shoulderLeft = {X: 0};
-            var shoulderRight = {X: 0};
+            var shoulderRight = {X: 0, Y: 0};
 
             // Get the data in JSON format.
             var jsonObject = JSON.parse(event.data);
@@ -42,6 +42,7 @@
                     }
                     else if (joint.name == "shoulderleft") {
                         shoulderLeft.X = joint.x;
+                        shoulderRight.Y = joint.y;
                     }
                     else if (joint.name == "shoulderright") {
                         shoulderRight.X = joint.x;
@@ -50,10 +51,10 @@
 
                     //////////////SUPPRIMER WIDGETS/////////////////////////
                     if (handLeft.X != 0 && handRight.X != 0) {
-                        if (handLeft.X <= handRight.X + 30 && handLeft.X >= handRight.X - 30) {
+                        if (handLeft.X <= handRight.X + 25 && handLeft.X >= handRight.X - 25) {
                             timer++;
                             //console.log("\n" + timer);
-                            if (timer >= 500) {
+                            if (timer >= 800) {
                                 timer = 0;
                                 document.getElementsByTagName('audio')[0].play();
                                 console.log("===============================================");
@@ -69,12 +70,13 @@
                         var tshirt = document.getElementById('tshirt');
                         var shopping = document.getElementById('shopping');
 
-                        var size = diff * 4;
+                        var size = diff * 4.15;
                         tshirt.style.width = '' + size + 'px';
                         tshirt.style.height = '' + size + 'px';
                         //console.log('width' + tshirt.width);
-                        shopping.style.left = '' + (shoulderLeft.X * 1.2) + 'px';
-                        console.log('LEFT' + shopping.style.left);
+                        shopping.style.left = '' + (shoulderLeft.X * 1.8) + 'px';
+                        shopping.style.top = '' + (shoulderRight.Y * 2.1) + 'px';
+                        //console.log('LEFT' + shopping.style.left);
                     }
                 }
             }
